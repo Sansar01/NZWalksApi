f6ad5f58c876ec4c0e7aea6de073852b2dc0211a
Sansar Tiwari <st9889477@gmail.com>
2025-03-20 17:49:40 +0530

    region controller

---
+++ NZWalksApi/Controllers/RegionsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalksApi.Data;
+using NZWalksApi.Models.Domain;
+
+namespace NZWalksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public RegionsController(NZWalksDbContext dbContextOptions)
+        {
+            this.nZWalksDbContext = dbContextOptions;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //var regions = new List<Region>
+            //{
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Auckland Region",
+            //        Code = "AKL",
+            //        RegionImageUrl = ""
+            //    },
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Wellington Region",
+            //        Code = "WL G",
+            //        RegionImageUrl = ""
+            //    }
+            //};
+
+            var regions = nZWalksDbContext.Regions.ToList();
+
+            return Ok(regions); 
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid Id)
+        {
+
+        }
+    }
+}
